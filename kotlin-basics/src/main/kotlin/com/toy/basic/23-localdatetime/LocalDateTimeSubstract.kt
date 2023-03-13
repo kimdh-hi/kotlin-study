731ad03729508677f9757a89339acd725c5b9ed1
daehyunKim <zbeld123@gmail.com>
2023-03-13 15:04:17 +0900

    feat(kotlin): defference of localdatetime

---
+++ kotlin-basics/src/main/kotlin/com/toy/basic/23-localdatetime/LocalDateTimeSubstract.kt
@@ -0,0 +1,12 @@
+package com.toy.basic.`23-localdatetime`
+
+import java.time.Duration
+import java.time.LocalDateTime
+
+fun main() {
+  val endDate = LocalDateTime.of(2023, 4, 1, 1, 3, 0)
+  val startDate = LocalDateTime.of(2023, 4, 1, 1, 1, 0)
+
+  val resultDuration = Duration.between(startDate, endDate)
+  println(resultDuration.seconds)
+}
