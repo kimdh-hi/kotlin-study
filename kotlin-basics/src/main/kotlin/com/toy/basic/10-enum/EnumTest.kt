40f290a2834429fed79c6bfb18fb817a19febd61
daehyunKim <zbeld123@gmail.com>
2023-03-14 10:49:11 +0900

    feat(kotlin): enum

--- kotlin-basics/src/main/kotlin/com/toy/basic/10-enum/EnumTest.kt
+++ kotlin-basics/src/main/kotlin/com/toy/basic/10-enum/EnumTest.kt
@@ -2,8 +2,23 @@ package com.toy.basic.`10-enum`
 
 fun main() {
   println(Idp.valueOf("google".uppercase()))
+
+  val topicA = Topic.TOPIC_A_FORMAT.ofPattern("key")
+  val topicB = Topic.TOPIC_B_FORMAT.ofPattern("key1", "key2")
+
+  println(topicA.format())
+  println(topicB.format())
 }
 
 enum class Idp {
   GOOGLE, NAVER
+}
+
+enum class Topic(val format: String) {
+  TOPIC_A_FORMAT("A/AA/%s"),
+  TOPIC_B_FORMAT("B/BB/%s/%s");
+
+  fun ofPattern(vararg keys: String): String {
+    return String.format(this.format, *keys)
+  }
 }
