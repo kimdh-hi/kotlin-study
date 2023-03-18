ec0e93fc2768ff848356026a74d88b7dfdd51e57
daehyunKim <zbeld123@gmail.com>
2023-03-18 17:33:35 +0900

    docs(kotlin): comments

--- kotlin-basics/src/main/kotlin/com/toy/basic/25-noarg/noargExtension.kt
+++ kotlin-basics/src/main/kotlin/com/toy/basic/25-noarg/noargExtension.kt
@@ -2,6 +2,14 @@ package com.toy.basic.`25-noarg`
 
 import com.toy.basic.common.NoArg
 
+/*
+noArg {
+    annotation("com.toy.basic.common.NoArg")
+    invokeInitializers = true
+}
+
+invokeInitializers = true 설정시 아래 코드 NPE 발생하지 않음.
+ */
 @NoArg
 data class NoArgTest(
   val a: String,
