2b5dd08aa11b4d53406f92bc46958a05135b60b8
daehyunKim <zbeld123@gmail.com>
2023-05-16 14:57:46 +0900

    feat(basic): list equals

---
+++ kotlin-basics/src/main/kotlin/com/toy/basic/08-collection/ListEquals.kt
@@ -0,0 +1,23 @@
+package com.toy.basic.`08-collection`
+
+fun main() {
+  val list1 = listOf("aaa", "bbb", "ccc")
+  val list2 = listOf("bbb", "aaa", "ccc")
+  val list3 = listOf("aaa", "aaa", "bbb", "ccc")
+  val list4 = listOf<String>()
+
+  println(list1 == list2) // xx
+  println(list1 == list2) // xx
+  println(list1.toTypedArray() contentEquals list2.toTypedArray()) // xx
+
+  println(list1.isEqual(list2))
+  println(list1.isEqual(list3))
+  println(list1.isEqual(list4))
+}
+
+fun <T> List<T>.isEqual(list: List<T>): Boolean {
+  if (this.size != list.size)
+    return false
+
+  return this.toSet() == list.toSet()
+}
