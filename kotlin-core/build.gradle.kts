2e5e1faf31750fcfae879fa98e6c4c39cb91abcb
daehyunKim <zbeld123@gmail.com>
2023-07-12 14:14:45 +0900

    chore(core): kotlin 1.9.0

--- kotlin-core/build.gradle.kts
+++ kotlin-core/build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.8.22"
-    kotlin("plugin.noarg") version "1.8.22"
+    kotlin("jvm") version "1.9.0"
+    kotlin("plugin.noarg") version "1.9.0"
 }
 
 noArg {
--- kotlin-core/src/main/kotlin/com/toy/core/10-enum/EnumEntriesFind.kt
+++ kotlin-core/src/main/kotlin/com/toy/core/10-enum/EnumEntriesFind.kt
@@ -2,13 +2,22 @@ package com.toy.core.`10-enum`
 
 // values() 를 대신해서 entries 를 사용하는 것을 권장
 fun main() {
-    println(findByNumber(1))
+    val v0Result = EnumEntriesFindTestEnum.findByNumberV0(1)
+    val v1Result = EnumEntriesFindTestEnum.findByNumber(1)
+
+    println(v0Result)
+    println(v1Result)
 }
 
 enum class EnumEntriesFindTestEnum(val number: Int) {
     ONE(1),
-    TWO(2)
+    TWO(2);
+
+    companion object {
+        fun findByNumberV0(number: Int) = EnumEntriesFindTestEnum.values().find { it.number == number }
+
+        fun findByNumber(number: Int) = EnumEntriesFindTestEnum.entries.find { it.number == number }
+    }
 }
 
-@OptIn(ExperimentalStdlibApi::class)
 fun findByNumber(number: Int): EnumEntriesFindTestEnum? = EnumEntriesFindTestEnum.entries.find { it.number == number }
