7699b9f37b8df049c857b0b3e58556fbc9060a6a
daehyunKim <zbeld123@gmail.com>
2023-07-26 18:59:09 +0900

    feat(core): localDateTime, ZonedDateTime convert utils

--- kotlin-core/src/main/kotlin/com/toy/core/38-ZonedDateTime/TimezoneDateUtils.kt
+++ kotlin-core/src/main/kotlin/com/toy/core/38-ZonedDateTime/TimezoneDateUtils.kt
@@ -4,12 +4,23 @@ import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.ZoneOffset
 import java.time.ZonedDateTime
+import java.time.zone.ZoneRulesException
 
 object TimezoneDateUtils {
 
   fun adjustTimezone(date: LocalDateTime, zoneId: ZoneId): LocalDateTime {
     val zonedDateTime = ZonedDateTime.of(date, ZoneOffset.UTC)
     return zonedDateTime.withZoneSameInstant(zoneId).toLocalDateTime()
+  }
+
+  fun getZoneId(zoneId: String) {
+    try {
+      ZoneId.of(zoneId)
+    } catch (e: ZoneRulesException) {
+      // 존재하지 않는 zoneId 의 경우 ZoneRulesException 발생
+      println("invalid zoneId...")
+      throw IllegalArgumentException("invalid zoneId...")
+    }
   }
 }
 
@@ -21,4 +32,9 @@ fun main() {
 
   println(now)
   println(adjustDate)
+
+  //####################
+
+  TimezoneDateUtils.getZoneId("America/New_York")
+  TimezoneDateUtils.getZoneId("zzzz")
 }
