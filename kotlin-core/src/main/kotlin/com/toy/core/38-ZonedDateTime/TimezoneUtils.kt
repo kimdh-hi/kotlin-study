9e8f382fababb4b4bf1926f34fa61d08742a914c
daehyunKim <zbeld123@gmail.com>
2023-09-01 10:18:41 +0900

    feat(core): timezoneUtils

--- kotlin-core/src/main/kotlin/com/toy/core/38-ZonedDateTime/TimezoneUtils.kt
+++ kotlin-core/src/main/kotlin/com/toy/core/38-ZonedDateTime/TimezoneUtils.kt
@@ -10,16 +10,19 @@ object TimezoneUtils {
     return zonedDateTime.withZoneSameInstant(zoneId).toLocalDateTime()
   }
 
-  fun adjustTimezone(timestamp: Long, zoneId: ZoneId): LocalDateTime {
-    return LocalDateTime.ofInstant(Instant.ofEpochMilli(timestamp), zoneId)
+  fun adjustTimezone(timestamp: Long, timezone: String): LocalDateTime {
+    return LocalDateTime.ofInstant(Instant.ofEpochMilli(timestamp), getZoneId(timezone))
   }
-  fun getZoneId(zoneId: String) {
-    try {
+  private fun getZoneId(zoneId: String): ZoneId {
+    return try {
       ZoneId.of(zoneId)
     } catch (e: ZoneRulesException) {
-      // 존재하지 않는 zoneId 의 경우 ZoneRulesException 발생
-      println("invalid zoneId...")
-      throw IllegalArgumentException("invalid zoneId...")
+      try {
+        ZoneId.of(ZoneId.SHORT_IDS[zoneId])
+      } catch (e: Exception) {
+        println("invalid zoneId...")
+        throw e
+      }
     }
   }
 }
@@ -36,9 +39,9 @@ fun main() {
   //####################
 
   println("===================================")
-  TimezoneUtils.getZoneId("America/New_York")
+//  TimezoneUtils.getZoneId("America/New_York")
   val timestamp = System.currentTimeMillis()
-  val adjustDate2 = TimezoneUtils.adjustTimezone(timestamp, zoneId)
+  val adjustDate2 = TimezoneUtils.adjustTimezone(timestamp, "EST")
   println(adjustDate2)
   println(LocalDateTime.now())
 }
