1c54f63dc031d247e0cfa23f11a43e9cc2b49aab
daehyunKim <zbeld123@gmail.com>
2023-09-08 22:25:51 +0900

    feat(core): context

--- kotlin-core/src/main/kotlin/com/toy/core/41-context/contextSample.kt
+++ kotlin-core/src/main/kotlin/com/toy/core/41-context/contextSample.kt
@@ -2,33 +2,38 @@ package com.toy.core.`41-context`
 
 fun main() {
   val logger = Logger("main")
+  val notificationSender = NotificationSender()
 //  store("test1", logger)
 //  store("test2", logger)
 //  store("test3", logger)
 
   with(logger) {
-    store("test1")
-    store("test2")
-    store("test3")
+    with(notificationSender) {
+      store("test1")
+      store("test2")
+      store("test3")
+    }
   }
 
 }
 
 class Logger(val name: String) {
-  fun log(s: String) {
-    println("[log] name: $s")
-  }
+  fun log(s: String) = println("[log] name: $s")
 
-  fun log2(s: String) {
-    println("[log2] name: $s")
-  }
+  fun log2(s: String) = println("[log2] name: $s")
+
+}
+
+class NotificationSender {
+  fun send(s: String) = println("notification send: $s")
 }
 
 //freeCompilerArgs = listOf("-Xcontext-receivers")
-context(Logger)
+context(Logger, NotificationSender)
 fun store(s: String) {
   log("store.. $s")
   log2("store.. $s")
+  send("send.. $s")
 }
 
 //fun Logger.store(s: String) {
