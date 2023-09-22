fadab0d2bf4b1241b58e69b85dc9f06fcd2ce19e
daehyunKim <zbeld123@gmail.com>
2023-09-22 14:18:09 +0900

    feat(core): multiple value let

--- kotlin-core/src/main/kotlin/com/toy/core/28-let/let2.kt
+++ kotlin-core/src/main/kotlin/com/toy/core/28-let/let2.kt
@@ -4,6 +4,12 @@ inline fun <T1 : Any, T2 : Any, R : Any> let2(p1: T1?, p2: T2?, block: (T1, T2) 
   return if (p1 != null && p2 != null) block(p1, p2) else null
 }
 
+inline fun <T: Any, R: Any> letMulti(vararg args: T?, block: (List<T>) -> R): R? {
+  return if (args.all { it != null }) {
+    block(args.filterNotNull())
+  } else null
+}
+
 fun main() {
   val data1: String? = "aaa"
   val data2: String? = "bbb"
@@ -20,6 +26,11 @@ fun main() {
 
   let2(data1, data2) { d1, d2 ->
     println("$d1, $d2")
+  }
+
+  val data3: String? = "ccc"
+  letMulti(data1, data2, data3) {
+    println(it)
   }
 }
 
