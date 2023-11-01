bf06736df7099fb29ae26c3a08520f24ac01b9ce
daehyunKim <zbeld123@gmail.com>
2023-11-01 09:03:41 +0900

    feat(core): object to map extension function style

--- kotlin-core/src/main/kotlin/com/toy/core/04-extension-function/ObjectToMap.kt
+++ kotlin-core/src/main/kotlin/com/toy/core/04-extension-function/ObjectToMap.kt
@@ -4,17 +4,32 @@ import java.time.LocalDateTime
 import kotlin.reflect.KClass
 import kotlin.reflect.full.memberProperties
 
-private fun <T : Any> toMap(target: T): Map<String, Any?> {
-  return (target::class as KClass<T>).memberProperties.associate { prop ->
-    prop.name to prop.get(target)?.let { value ->
+//private fun <T : Any> toMap(target: T): Map<String, Any?> {
+//  return (target::class as KClass<T>).memberProperties.associate { prop ->
+//    prop.name to prop.get(target)?.let { value ->
+//      if (value::class.isData) {
+//        toMap(value)
+//      } else {
+//        value
+//      }
+//    }
+//  }
+//}
+
+// 확장함수 스타일
+fun <T : Any> T.toMap(): Map<String, Any?> {
+  val clazz = this::class as KClass<T>
+  return clazz.memberProperties.associate { prop ->
+    prop.name to prop.get(this)?.let { value ->
       if (value::class.isData) {
-        toMap(value)
+        value.toMap()
       } else {
         value
       }
     }
   }
 }
+
 
 data class Test(
   val data: String,
@@ -30,6 +45,10 @@ data class TestInner(
 
 fun main() {
   val test = Test("data", LocalDateTime.now(), 10, TestInner("data1", "data2"))
-  val map = toMap(test)
-  println(map)
+
+//  val map1 = toMap(test)
+//  println(map1)
+
+  val map2 = test.toMap()
+  println(map2)
 }
