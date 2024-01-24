acb9d4dcdc5ad3aa1fa04a5b413e6c0f4ee046e5
daehyunKim <zbeld123@gmail.com>
2024-01-24 09:14:43 +0900

    docs(core): Result

--- kotlin-core/src/main/kotlin/com/toy/core/12-exception/ResultType.kt
+++ kotlin-core/src/main/kotlin/com/toy/core/12-exception/ResultType.kt
@@ -2,6 +2,8 @@ package com.toy.core.`12-exception`
 
 class ExternalService {
 
+  // exception 처리를 클라이언트 코드에 위임
+  // Result 는 결과가 성공인 경우는 반환값을 그대로 value로 하고 실패인 경우 Throwable 를 갖는다.
   fun checkOdd(num: Int): Result<Int> = if (num % 2 != 0) {
     Result.success(num)
   } else {
