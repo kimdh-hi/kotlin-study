f15478308c9984647e8b993d26f6fef2f280e469
daehyunKim <zbeld123@gmail.com>
2024-04-02 10:08:04 +0900

    feat(core): testDouble

---
+++ kotlin-core/src/main/kotlin/com/toy/core/63-testDouble/Dummy.kt
@@ -0,0 +1,11 @@
+package com.toy.core.`63-testDouble`
+
+interface Printer {
+  fun print(text: String)
+}
+
+class DummyPrinter: Printer {
+  override fun print(text: String) {
+    TODO("Not yet implemented")
+  }
+}
