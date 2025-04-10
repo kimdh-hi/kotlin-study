13bf5bb8d56d98a2a9b4155bd3e4b79aa60dc905
daehyunKim <zbeld123@gmail.com>
2025-04-10 10:52:54 +0900

    feat(core):zip

--- kotlin-core/src/main/kotlin/com/toy/core/08-collection/Zip.kt
+++ kotlin-core/src/main/kotlin/com/toy/core/08-collection/Zip.kt
@@ -1,12 +1,13 @@
 package com.toy.core.`08-collection`
 
+// zip 시 리스트 원소 수가 다른 경우 적은 쪽 기준으로 zip
 fun main() {
 
   val list1 = listOf("a", "b", "c")
-  val list2 = listOf(1, 2, 3)
+  val list2 = listOf(1, 2)
 
   // [("a", "1"), ("b", "2"), ("c", "3")]
-  val zippedList = list1 zip list2
+  val zippedList = list1 zip list2 // list1.zip(list2)
   println(zippedList)
 
   list1.zip(list2) { alp, num ->
