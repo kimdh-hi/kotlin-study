ce7cb55858dcafcf0f9e0aa6e52415c4c458ea8d
daehyunKim <zbeld123@gmail.com>
2025-04-28 11:14:18 +0900

    feat(core):value class sample

--- kotlin-core/src/main/kotlin/com/toy/core/0-a/Test.kt
+++ kotlin-core/src/main/kotlin/com/toy/core/0-a/Test.kt
@@ -1,8 +1,17 @@
 package com.toy.core.`0-a`
 
 import java.io.File
+import java.time.LocalDate
+import java.time.LocalDateTime
 
 fun main() {
 
 }
 
+
+
+fun MutableList<Int>.swap(index1: Int, index2: Int) {
+  val tmp = this[index1] // 'this' corresponds to the list
+  this[index1] = this[index2]
+  this[index2] = tmp
+}
--- kotlin-core/src/main/kotlin/com/toy/core/34-value-class/Sample.kt
+++ kotlin-core/src/main/kotlin/com/toy/core/34-value-class/Sample.kt
@@ -11,12 +11,49 @@ package com.toy.core.`34-value-class`
  */
 
 @JvmInline
-value class Number(private val number: Int)
+value class EvenNumber(private val number: Int) {
+    init { require(number % 2 == 0) { "number must be even. number=$number" } }
+}
 
 fun main() {
-    val number1 = Number(1)
-    val number2 = Number(1)
+    val number1 = EvenNumber(2)
+    val number2 = EvenNumber(2)
     println(number1 == number2) // true
 
-// println(number1 === number2) // === 연산자(객체 동등성) 지원 x (객체 자체를 생성하지 않는다고 하니 의미없는 연산자임)
+    val number3 = EvenNumber(1) // IllegalArgumentException
 }
+
+// println(number1 === number2) // === 연산자(객체 동등성) 지원 x (객체 자체를 생성하지 않는다고 하니 의미없는 연산자임)
+
+/*
+// .java compile
+public final class EvenNumber {
+   private final int number;
+
+   //.. toString(), equalsAndHashCode, ...
+
+   public static int constructor_impl(int number) {
+      if (number % 2 != 0) {
+         int var2 = 0;
+         String var3 = "number must be even. number=" + number;
+         throw new IllegalArgumentException(var3.toString());
+      } else {
+         return number;
+      }
+   }
+}
+
+public final class SampleKt {
+   public static final void main() {
+      int number1 = EvenNumber.constructor-impl(2);
+      int number2 = EvenNumber.constructor-impl(2);
+      int number3 = EvenNumber.equals-impl0(number1, number2);
+      System.out.println((boolean)number3);
+      number3 = EvenNumber.constructor-impl(1);
+   }
+
+   public static void main(String[] args) {
+      main();
+   }
+}
+ */
